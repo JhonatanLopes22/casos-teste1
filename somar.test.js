9f644170568f01b8f1aefe878664c4eeb13f3b2b
Jhonatan Lopes <jhonatanlopes22@hotmail.com>
2024-05-24 22:16:40 -0300

    somar

---
+++ somar.test.js
@@ -0,0 +1,10 @@
+const somar = require("./somar")
+
+test('somar 1 + 2 é igual a 3', ()=>{
+    expect(somar(1,2)).toBe(3)
+})
+
+test('Soma de Dois Números Inteiros',()=>{
+    expect(somar(3,5)).toBe(8)
+})
+
